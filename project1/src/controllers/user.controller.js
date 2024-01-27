5de4f09bde03f9e94e1a604649c564fe15678b3e
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-01-28 02:35:16 +0530

    Problem fixed!!

--- project1/src/controllers/user.controller.js
+++ project1/src/controllers/user.controller.js
@@ -1,12 +1,12 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import path from "path"; 
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.sendfile(
-    path.join(__dirname, "..", "..", "public", "temp", "index.html")
-  );
-  // const {username, email, password} = req.body;
-  // console.log("email :", email);
+  // res.sendfile(
+  //   path.join(__dirname, "..", "..", "public", "temp", "index.html")
+  // );
+  const {username, email, password} = req.body;
+  console.log("email :", email);
 });
 
 export { registerUser };
